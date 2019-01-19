dc924d111313f74a07ecadbb4e1a5855f09c4a89
Chres <clc8680@rit.edu>
2019-01-19 14:47:12 -0500

    Cleanup/sysargv

--- CPC.py
+++ CPC.py
@@ -1,12 +1,12 @@
 import requests
-import json
-
+import sys
 
 #FILL THESE IN FIRST
 api_key = ''   #From Challonge Developer API
 username = ''  #Account Username
-subdomain = '' #Subdomain (ex: rocnysmash)
-tournament_url = ''  #url for the tournament, after challonge.com/{url}
+subdomain = 'rocnysmash' #Subdomain (ex: rocnysmash)
+tournament_url = sys.argv[1]  #url for the tournament, after challonge.com/{url}
+
 
 
 # For clearing entrants
@@ -14,6 +14,10 @@ tournament_url = ''  #url for the tournament, after challonge.com/{url}
 # response = requests.delete(url)
 
 if __name__ == '__main__':
+
+    print("Challonge settings: ")
+    print("Set number of entrants in each pool to total entrants/8 rounded up")
+
 
     url = "https://" + username + ":" + api_key + "@api.challonge.com/v1/tournaments/" + subdomain + "-" + tournament_url + "/participants.json"
     response = requests.get(url).json()
@@ -27,9 +31,7 @@ if __name__ == '__main__':
     count = len(seeded_entrants)
 
     blank_slots = 8 - (count % 8)
-    print("Blank Slots" + str(blank_slots))
     for i in range(0, blank_slots):
-        print(str(i))
         seeded_entrants.append("bye" + str(i))
 
     count = len(seeded_entrants)
@@ -38,7 +40,7 @@ if __name__ == '__main__':
         index_list = []
         for j in range(i, count, 8):
             seeded_entrants_copy = seeded_entrants.copy()
-            print(str(j+1) + ": " + seeded_entrants_copy[j])
+            #print(str(j+1) + ": " + seeded_entrants_copy[j])
             index_list.append(seeded_entrants_copy.pop(j))
         pools.extend(index_list)
 
@@ -46,3 +48,4 @@ if __name__ == '__main__':
     for line in pools:
         f.write(line + '\n')
 
+    print("Check files/output.txt for bulk add text")
